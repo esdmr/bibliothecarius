87eeb06a67f16cfb1ac21fa42f07c06a3edb2a68
esdmr <esdmr0@gmail.com>
2024-06-11 20:00:54 +0330

    Always enable venv in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from os.path import dirname, join
 from sys import path
 
+venv_activate_this = join(dirname(__file__), ".venv", "bin", "activate_this.py")
+exec(open(venv_activate_this).read(), {"__file__": venv_activate_this})
+
 path.append(join(dirname(__file__), "src"))
 
 from bibliothecarius_backend import app  # type: ignore
