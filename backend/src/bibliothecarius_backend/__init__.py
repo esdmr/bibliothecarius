2d6ef724233be3265293b7e53faacb3ffbbc75ed
esdmr <esdmr0@gmail.com>
2024-06-12 12:52:07 +0330

    Add redirect to frontend

--- backend/src/bibliothecarius_backend/__init__.py
+++ backend/src/bibliothecarius_backend/__init__.py
@@ -1,9 +1,16 @@
+from flask import redirect
 from .flask import app
 from . import models
 from . import schemas
 from . import account
 from . import challenge
 from . import api
+
+
+@app.route("/")
+def index():
+    return redirect("https://esdmr.ir/bibliothecarius/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
